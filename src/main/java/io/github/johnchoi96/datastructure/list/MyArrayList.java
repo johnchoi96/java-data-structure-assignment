d7f390b973a8abe4dbfe684ced8429d4ee6df488
John Choi <johnchoi1003@icloud.com>
2024-08-05 01:07:07 -0400

    Doc updated, answer key updated, fixed broken test case

--- src/main/java/io/github/johnchoi96/datastructure/list/MyArrayList.java
+++ src/main/java/io/github/johnchoi96/datastructure/list/MyArrayList.java
@@ -1,29 +1,63 @@
 package io.github.johnchoi96.datastructure.list;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 public class MyArrayList<E> implements MyList<E> {
+
+    private Object[] internalList;
+
+    private int capacity;
+
+    private int size;
+
+    public MyArrayList() {
+        capacity = 20;
+        size = 0;
+        internalList = new Object[capacity];
+    }
 
     /**
      * Adds element at the end of the list.
      * For example, adding [1, 2, 3, 4] in order should result in:
      * [1, 2, 3, 4]
+     *
      * @param element to add
      */
     @Override
     public void add(E element) {
-        throw new NotImplementedException("Not Implemented");
+        if (size >= capacity) {
+            resizeInternalList();
+        }
+        internalList[size] = element;
+        size++;
     }
 
     /**
      * Adds element at the front of the list.
      * For example, adding [1, 2, 3, 4] in order should result in:
      * [4, 3, 2, 1]
+     *
      * @param element to add
      */
     @Override
     public void addAtFront(E element) {
-        throw new NotImplementedException("Not Implemented");
+        if (size >= capacity) {
+            resizeInternalList();
+        }
+        // shift everything to the right by one index
+        for (int i = size; i > 0; i--) {
+            internalList[i] = internalList[i - 1];
+        }
+        internalList[0] = element;
+        size++;
+    }
+
+    private void resizeInternalList() {
+        int newCapacity = capacity * 2;
+        Object[] temp = new Object[newCapacity];
+        for (int i = 0; i < internalList.length; i++) {
+            temp[i] = internalList[i];
+        }
+        internalList = temp;
+        capacity = newCapacity;
     }
 
     /**
@@ -33,26 +67,41 @@ public class MyArrayList<E> implements MyList<E> {
      * with removal index of 2 should result in:
      * [1, 2, 4]
      * If index is invalid, throw IllegalArgumentException.
+     *
      * @param index removal index
      * @return removed element
      */
     @Override
     public E remove(int index) {
-        throw new NotImplementedException("Not Implemented");
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        Object removedObject = internalList[index];
+        for (int i = index; i < size; i++) {
+            internalList[i] = internalList[i + 1];
+        }
+        size--;
+        return (E) removedObject;
     }
 
     /**
-     * Removes an element from the front of the list.
+     * Removes an element from the end of the list.
      * For example, array list with:
      * [1, 2, 3, 4]
      * should result in:
-     * [2, 3, 4]
+     * [1, 2, 3]
      * If removal is invalid, throw IllegalStateException.
+     *
      * @return removed element
      */
     @Override
     public E remove() {
-        throw new NotImplementedException("Not Implemented");
+        if (size == 0) {
+            throw new IllegalStateException();
+        }
+        Object removedObject = internalList[size - 1];
+        size--;
+        return (E) removedObject;
     }
 
     /**
@@ -62,22 +111,26 @@ public class MyArrayList<E> implements MyList<E> {
      * with given index of 2 should result in:
      * 3
      * If index is invalid, throw IllegalArgumentException.
+     *
      * @param index of the element to retrieve
      * @return element at the given index
      */
     @Override
     public E get(int index) {
-        throw new NotImplementedException("Not Implemented");
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        return (E) internalList[index];
     }
 
     @Override
     public int getSize() {
-        throw new NotImplementedException("Not Implemented");
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new NotImplementedException("Not Implemented");
+        return getSize() == 0;
     }
 
     /**
@@ -88,12 +141,18 @@ public class MyArrayList<E> implements MyList<E> {
      * should result in:
      * [1, 2, -5, 4]
      * Throw an IllegalArgumentException if index is invalid.
-     * @param index of the list to replace
+     *
+     * @param index   of the list to replace
      * @param element new element
      * @return original element
      */
     @Override
     public E replace(int index, E element) {
-        throw new NotImplementedException("Not Implemented");
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        Object originalElement = internalList[index];
+        internalList[index] = element;
+        return (E) originalElement;
     }
 }
